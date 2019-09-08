ff450b1b1d4685d6e5194bf50e75e59dcaba5272
xiaochou12138 <54838374+xiaochou12138@users.noreply.github.com>
2019-09-08 21:04:01 +0800

    Create ShapeDlg.java

---
+++ ShapeDlg.java
@@ -0,0 +1,38 @@
+package company;
+import java.awt.*;
+
+import java.Dialog.*;
+public class ShapeDlg extends Dialog{
+
+
+    public void finalizee(){}
+
+    protected void DoDataExchange(){}
+
+    public boolean OnInilDialog()
+
+    {
+        return true;
+    }
+    public int m_Type;
+
+    public int m_x;
+
+    public int m_y;
+
+    public int m_w;
+
+    public int m_h;
+
+    String m_text;
+
+    public int m_pen_width;
+
+    public int m_pen_type;
+
+    public int m_brush_type;
+
+    public Color m_pen_color;
+
+    public Color m_brush_color;
+}
